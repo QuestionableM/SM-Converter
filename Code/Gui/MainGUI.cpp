4c8bc8a5bfdeb7ad41e4e409094c0f8f4336182f
QuestionableM <77170113+QuestionableM@users.noreply.github.com>
2023-01-10 18:29:22 +0300

    Created an empty project

---
+++ Code/Gui/MainGUI.cpp
@@ -0,0 +1,2 @@
+#include "MainGUI.h"
+
---
+++ Code/main.cpp
@@ -0,0 +1,12 @@
+#include <Windows.h>
+
+int WINAPI WinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR lpCmdLine,
+    _In_ int nShowCmd)
+{
+
+
+	return 0;
+}
