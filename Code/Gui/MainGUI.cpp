929c809e74939a1fe3b6776d18d38de91d84f7d2
QuestionableM <77170113+QuestionableM@users.noreply.github.com>
2023-01-11 15:08:25 -0700

    Added database loader thread and object loader thread

--- Code/Gui/MainGUI.cpp
+++ Code/Gui/MainGUI.cpp
@@ -1,6 +1,12 @@
 #include "MainGui.h"
 #include "AboutGui.h"
 
+#include "ObjectDatabase\KeywordReplacer.hpp"
+#include "ObjectDatabase\ObjectDatabase.hpp"
+#include "ObjectDatabase\DatabaseConfig.hpp"
+#include "ObjectDatabase\ProgCounter.hpp"
+#include "ObjectDatabase\Mods\Mod.hpp"
+
 #include "Utils\Console.hpp"
 #include "Utils\File.hpp"
 
@@ -22,11 +28,42 @@ namespace SMConverter
 		this->InitializeComponent();
 
 		m_cb_selectedGenerator->SelectedIndex = 0;
+
+		DatabaseLoader::InitializeDatabase();
 	}
 
 	MainGui::~MainGui()
 	{
 		if (components) delete components;
+	}
+
+	void MainGui::MainGui_Shown(System::Object^ sender, System::EventArgs^ e)
+	{
+		namespace WForms = System::Windows::Forms;
+
+		DebugOutL("MainGui has been loaded!");
+
+		if (DatabaseConfig::GamePath.empty() || !File::Exists(DatabaseConfig::GamePath))
+		{
+			const WForms::DialogResult v_dr = WForms::MessageBox::Show(
+				"The program couldn't find a path to Scrap Mechanic.\n\nWould you like to specify the path to Scrap Mechanic by yourself?",
+				"No Game Path",
+				WForms::MessageBoxButtons::YesNo,
+				WForms::MessageBoxIcon::Question
+			);
+
+			if (v_dr == WForms::DialogResult::Yes)
+			{
+				DebugOutL("TODO: Make an options gui");
+			}
+			else
+				this->Close();
+
+			return;
+		}
+
+		this->LoadObjectDatabase(false);
+		this->LoadUserObjects();
 	}
 
 	void MainGui::SelectedGenerator_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e)
@@ -48,6 +85,9 @@ namespace SMConverter
 		};
 
 		m_tb_searchBox->Clear();
+		
+		if (!m_obj_isLoaded)
+			this->UpdateObjectListStatus();
 
 		SendMessage(
 			static_cast<HWND>(m_tb_path->Handle.ToPointer()),
@@ -104,5 +144,133 @@ namespace SMConverter
 
 		m_lbl_objSelectorStatus->Left = m_lb_objectSelector->Left + (v_selector_sz.Width / 2) - (v_lbl_sz.Width / 2);
 		m_lbl_objSelectorStatus->Top = m_lb_objectSelector->Top + (v_selector_sz.Height / 2) - (v_lbl_sz.Height / 2);
+	}
+
+	void MainGui::MainGui_Timer_UpdateProgressBar(System::Object^ sender, System::EventArgs^ e)
+	{
+		if (ProgCounter::State == ProgState::None)
+			return;
+
+		std::wstring v_state_output = ProgCounter::GetStateString();
+		if (ProgCounter::StateHasNumbers())
+		{
+			const std::size_t v_max_val = ProgCounter::ProgressMax;
+			const std::size_t v_cur_val = std::min(ProgCounter::ProgressValue, v_max_val);
+
+			m_pb_progress->Maximum = static_cast<int>(v_max_val);
+			m_pb_progress->Value   = static_cast<int>(v_cur_val);
+
+			//Append numbers to the string
+			v_state_output.append(L"(");
+			v_state_output.append(std::to_wstring(v_cur_val));
+			v_state_output.append(L" / ");
+			v_state_output.append(std::to_wstring(v_max_val));
+			v_state_output.append(L")");
+		}
+
+		m_lbl_progressStatus->Text = gcnew System::String(v_state_output.c_str());
+	}
+
+	void MainGui::LoadObjectDatabase(const bool& should_reload)
+	{
+		if (m_bw_databaseLoader->IsBusy) return;
+
+		DebugOutL(0b0011_fg, "Loading object database...");
+
+		this->MainGui_ChangeGuiState(false, m_obj_isLoaded, true);
+		m_progressBarUpdater->Start();
+		m_bw_databaseLoader->RunWorkerAsync(should_reload);
+	}
+
+	void MainGui::MainGui_DatabaseLoader_DoWork(System::Object^ sender, System::ComponentModel::DoWorkEventArgs^ e)
+	{
+		const bool v_should_update = safe_cast<bool>(e->Argument);
+		if (v_should_update)
+			KeywordReplacer::Initialize();
+
+		DatabaseLoader::LoadDatabase();
+	}
+
+	void MainGui::MainGui_DatabaseLoader_RunWorkerCompleted(System::Object^ sender, System::ComponentModel::RunWorkerCompletedEventArgs^ e)
+	{
+		m_progressBarUpdater->Stop();
+		this->MainGui_ChangeGuiState(true, m_obj_isLoaded, true);
+
+		ProgCounter::SetState(ProgState::None, 0);
+
+		m_pb_progress->Value = 0;
+		m_pb_progress->Maximum = 0;
+
+		const std::size_t v_mod_count = Mod::GetAmountOfMods();
+		const std::size_t v_obj_count = Mod::GetAmountOfObjects();
+
+		const std::wstring v_load_msg = L"Successfully loaded " + std::to_wstring(v_obj_count) + L" objects from " + std::to_wstring(v_mod_count) + L" mods";
+		m_lbl_progressStatus->Text = gcnew System::String(v_load_msg.c_str());
+	}
+
+	void MainGui::MainGui_ChangeGuiState(bool db_loaded, bool objlist_loaded, bool obj_converted)
+	{
+		m_database_isLoaded = db_loaded;
+		m_obj_isLoaded = objlist_loaded;
+
+		const bool objlist_and_obj_loaded = objlist_loaded && obj_converted;
+		const bool db_loaded_and_obj_converted = db_loaded && obj_converted;
+
+		m_tb_path->Enabled = db_loaded_and_obj_converted;
+		m_btn_folderDialog->Enabled = db_loaded_and_obj_converted;
+		m_tb_searchBox->Enabled = objlist_and_obj_loaded;
+
+		m_btn_reloadUserObjects->Enabled = objlist_and_obj_loaded;
+		m_btn_reloadDatabase->Enabled = db_loaded_and_obj_converted;
+		m_lb_objectSelector->Enabled = objlist_and_obj_loaded;
+
+		m_btn_convert->Enabled = db_loaded_and_obj_converted && (m_tb_path->TextLength > 0);
+		m_btn_options->Enabled = db_loaded_and_obj_converted;
+	}
+
+	void MainGui::MainGui_ReloadDatabase_Click(System::Object^ sender, System::EventArgs^ e)
+	{
+		this->LoadObjectDatabase(true);
+	}
+
+	void MainGui::LoadUserObjects()
+	{
+		if (m_bw_objectLoader->IsBusy) return;
+
+		m_lbl_objSelectorStatus->Visible = true;
+		this->UpdateObjectListStatus();
+
+		this->MainGui_ChangeGuiState(m_database_isLoaded, false, true);
+		m_bw_objectLoader->RunWorkerAsync();
+	}
+
+	void MainGui::UpdateObjectListStatus()
+	{
+		const static wchar_t* g_objectListStatusMessages[] =
+		{
+			L"Loading Blueprints...",
+			L"Loading Tiles...",
+			L"Loading Scripts..."
+		};
+
+		const wchar_t* v_cur_message = g_objectListStatusMessages[m_cb_selectedGenerator->SelectedIndex];
+		m_lbl_objSelectorStatus->Text = gcnew System::String(v_cur_message);
+	}
+
+	void MainGui::MainGui_ObjectLoader_DoWork(System::Object^ sender, System::ComponentModel::DoWorkEventArgs^ e)
+	{
+		//TODO: Actually load blueprints, tiles and scripts in here
+		Sleep(2000);
+	}
+
+	void MainGui::MainGui_ObjectLoader_RunWorkerCompleted(System::Object^ sender, System::ComponentModel::RunWorkerCompletedEventArgs^ e)
+	{
+		m_lbl_objSelectorStatus->Visible = false;
+		this->MainGui_ChangeGuiState(m_database_isLoaded, true, true);
+	}
+
+	void MainGui::MainGui_ReloadUserObjects_Click(System::Object^ sender, System::EventArgs^ e)
+	{
+		this->LoadUserObjects();
 	}
 }
--- Code/Gui/MainGUI.h
+++ Code/Gui/MainGUI.h
@@ -1,7 +1,7 @@
 #pragma once
 
-namespace SMConverter {
-
+namespace SMConverter
+{
 	using namespace System;
 	using namespace System::ComponentModel;
 	using namespace System::Collections;
@@ -9,9 +9,6 @@ namespace SMConverter {
 	using namespace System::Data;
 	using namespace System::Drawing;
 
-	/// <summary>
-	/// Summary for MainGui
-	/// </summary>
 	public ref class MainGui : public System::Windows::Forms::Form
 	{
 	public:
@@ -20,50 +17,41 @@ namespace SMConverter {
 	protected:
 		~MainGui();
 
-	private: System::Windows::Forms::Button^ m_btn_folderDialog;
-	private: System::Windows::Forms::Button^ m_btn_convert;
-	protected:
-
-	protected:
-
-	private: System::Windows::Forms::TextBox^ m_tb_path;
-	private: System::Windows::Forms::TextBox^ m_tb_searchBox;
-	private: System::Windows::Forms::ListBox^ m_lb_objectSelector;
-
-
-
-	private: System::Windows::Forms::ComboBox^ m_cb_selectedGenerator;
-	private: System::Windows::Forms::Label^ m_lbl_generatorType;
-	private: System::Windows::Forms::ProgressBar^ m_pb_progress;
-	private: System::Windows::Forms::Label^ m_lbl_progressStatus;
-	private: System::Windows::Forms::MenuStrip^ m_menuStrip;
-	private: System::Windows::Forms::ToolStripMenuItem^ m_btn_aboutProgram;
-
-	private: System::Windows::Forms::ToolStripMenuItem^ settingsToolStripMenuItem;
-	private: System::Windows::Forms::ToolStripMenuItem^ reloadObjectDatabaseToolStripMenuItem;
-	private: System::Windows::Forms::ToolStripSeparator^ toolStripSeparator1;
-	private: System::Windows::Forms::ToolStripMenuItem^ optionsToolStripMenuItem;
-	private: System::Windows::Forms::Label^ m_lbl_objSelectorStatus;
-
-
-
-
-
-
-
 	private:
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		System::ComponentModel::Container ^components;
+		bool m_obj_isLoaded = false;
+		bool m_database_isLoaded = false;
+
+		System::Windows::Forms::Button^ m_btn_folderDialog;
+		System::Windows::Forms::Button^ m_btn_convert;
+
+		System::Windows::Forms::ToolStripSeparator^ toolStripSeparator1;
+		System::Windows::Forms::ToolStripMenuItem^ m_btn_aboutProgram;
+		System::Windows::Forms::ToolStripMenuItem^ settingsToolStripMenuItem;
+		System::Windows::Forms::ToolStripMenuItem^ m_btn_reloadDatabase;
+		System::Windows::Forms::ToolStripMenuItem^ m_btn_options;
+		System::Windows::Forms::ToolStripMenuItem^ m_btn_reloadUserObjects;
+
+		System::Windows::Forms::TextBox^ m_tb_path;
+		System::Windows::Forms::TextBox^ m_tb_searchBox;
+		System::Windows::Forms::ListBox^ m_lb_objectSelector;
+		System::Windows::Forms::ComboBox^ m_cb_selectedGenerator;
+
+		System::Windows::Forms::Label^ m_lbl_generatorType;
+		System::Windows::Forms::Label^ m_lbl_progressStatus;
+		System::Windows::Forms::Label^ m_lbl_objSelectorStatus;
+
+		System::ComponentModel::BackgroundWorker^ m_bw_databaseLoader;
+		System::ComponentModel::BackgroundWorker^ m_bw_objectLoader;
+
+		System::Windows::Forms::ProgressBar^ m_pb_progress;
+		System::Windows::Forms::MenuStrip^ m_menuStrip;
+		System::Windows::Forms::Timer^ m_progressBarUpdater;
+		System::ComponentModel::IContainer^ components;
 
 #pragma region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->components = (gcnew System::ComponentModel::Container());
 			this->m_btn_folderDialog = (gcnew System::Windows::Forms::Button());
 			this->m_btn_convert = (gcnew System::Windows::Forms::Button());
 			this->m_tb_path = (gcnew System::Windows::Forms::TextBox());
@@ -76,10 +64,14 @@ namespace SMConverter {
 			this->m_menuStrip = (gcnew System::Windows::Forms::MenuStrip());
 			this->m_btn_aboutProgram = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->settingsToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
-			this->reloadObjectDatabaseToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->m_btn_reloadUserObjects = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->m_btn_reloadDatabase = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->toolStripSeparator1 = (gcnew System::Windows::Forms::ToolStripSeparator());
-			this->optionsToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->m_btn_options = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->m_lbl_objSelectorStatus = (gcnew System::Windows::Forms::Label());
+			this->m_progressBarUpdater = (gcnew System::Windows::Forms::Timer(this->components));
+			this->m_bw_databaseLoader = (gcnew System::ComponentModel::BackgroundWorker());
+			this->m_bw_objectLoader = (gcnew System::ComponentModel::BackgroundWorker());
 			this->m_menuStrip->SuspendLayout();
 			this->SuspendLayout();
 			// 
@@ -87,7 +79,7 @@ namespace SMConverter {
 			// 
 			this->m_btn_folderDialog->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
 			this->m_btn_folderDialog->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F));
-			this->m_btn_folderDialog->Location = System::Drawing::Point(328, 27);
+			this->m_btn_folderDialog->Location = System::Drawing::Point(428, 27);
 			this->m_btn_folderDialog->Name = L"m_btn_folderDialog";
 			this->m_btn_folderDialog->Size = System::Drawing::Size(44, 22);
 			this->m_btn_folderDialog->TabIndex = 0;
@@ -100,7 +92,7 @@ namespace SMConverter {
 			this->m_btn_convert->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Right));
 			this->m_btn_convert->Enabled = false;
 			this->m_btn_convert->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F));
-			this->m_btn_convert->Location = System::Drawing::Point(282, 263);
+			this->m_btn_convert->Location = System::Drawing::Point(382, 263);
 			this->m_btn_convert->Name = L"m_btn_convert";
 			this->m_btn_convert->Size = System::Drawing::Size(90, 36);
 			this->m_btn_convert->TabIndex = 1;
@@ -115,7 +107,7 @@ namespace SMConverter {
 			this->m_tb_path->Location = System::Drawing::Point(12, 27);
 			this->m_tb_path->MaxLength = 256;
 			this->m_tb_path->Name = L"m_tb_path";
-			this->m_tb_path->Size = System::Drawing::Size(310, 22);
+			this->m_tb_path->Size = System::Drawing::Size(410, 22);
 			this->m_tb_path->TabIndex = 2;
 			this->m_tb_path->TextChanged += gcnew System::EventHandler(this, &MainGui::PathTextBox_TextChanged);
 			// 
@@ -127,7 +119,7 @@ namespace SMConverter {
 			this->m_tb_searchBox->Location = System::Drawing::Point(12, 55);
 			this->m_tb_searchBox->MaxLength = 64;
 			this->m_tb_searchBox->Name = L"m_tb_searchBox";
-			this->m_tb_searchBox->Size = System::Drawing::Size(360, 22);
+			this->m_tb_searchBox->Size = System::Drawing::Size(460, 22);
 			this->m_tb_searchBox->TabIndex = 3;
 			// 
 			// m_lb_objectSelector
@@ -141,7 +133,7 @@ namespace SMConverter {
 			this->m_lb_objectSelector->ItemHeight = 16;
 			this->m_lb_objectSelector->Location = System::Drawing::Point(12, 83);
 			this->m_lb_objectSelector->Name = L"m_lb_objectSelector";
-			this->m_lb_objectSelector->Size = System::Drawing::Size(360, 128);
+			this->m_lb_objectSelector->Size = System::Drawing::Size(460, 128);
 			this->m_lb_objectSelector->TabIndex = 4;
 			// 
 			// m_cb_selectedGenerator
@@ -157,7 +149,7 @@ namespace SMConverter {
 			});
 			this->m_cb_selectedGenerator->Location = System::Drawing::Point(12, 233);
 			this->m_cb_selectedGenerator->Name = L"m_cb_selectedGenerator";
-			this->m_cb_selectedGenerator->Size = System::Drawing::Size(360, 24);
+			this->m_cb_selectedGenerator->Size = System::Drawing::Size(460, 24);
 			this->m_cb_selectedGenerator->TabIndex = 5;
 			this->m_cb_selectedGenerator->SelectedIndexChanged += gcnew System::EventHandler(this, &MainGui::SelectedGenerator_SelectedIndexChanged);
 			// 
@@ -178,7 +170,7 @@ namespace SMConverter {
 				| System::Windows::Forms::AnchorStyles::Right));
 			this->m_pb_progress->Location = System::Drawing::Point(12, 284);
 			this->m_pb_progress->Name = L"m_pb_progress";
-			this->m_pb_progress->Size = System::Drawing::Size(264, 15);
+			this->m_pb_progress->Size = System::Drawing::Size(364, 15);
 			this->m_pb_progress->TabIndex = 7;
 			// 
 			// m_lbl_progressStatus
@@ -201,7 +193,7 @@ namespace SMConverter {
 			this->m_menuStrip->Location = System::Drawing::Point(0, 0);
 			this->m_menuStrip->Name = L"m_menuStrip";
 			this->m_menuStrip->RenderMode = System::Windows::Forms::ToolStripRenderMode::System;
-			this->m_menuStrip->Size = System::Drawing::Size(384, 24);
+			this->m_menuStrip->Size = System::Drawing::Size(484, 24);
 			this->m_menuStrip->TabIndex = 10;
 			this->m_menuStrip->Text = L"Menu Strip";
 			// 
@@ -214,46 +206,70 @@ namespace SMConverter {
 			// 
 			// settingsToolStripMenuItem
 			// 
-			this->settingsToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
-				this->reloadObjectDatabaseToolStripMenuItem,
-					this->toolStripSeparator1, this->optionsToolStripMenuItem
+			this->settingsToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(4) {
+				this->m_btn_reloadUserObjects,
+					this->m_btn_reloadDatabase, this->toolStripSeparator1, this->m_btn_options
 			});
 			this->settingsToolStripMenuItem->Name = L"settingsToolStripMenuItem";
 			this->settingsToolStripMenuItem->Size = System::Drawing::Size(61, 20);
 			this->settingsToolStripMenuItem->Text = L"Settings";
 			// 
-			// reloadObjectDatabaseToolStripMenuItem
+			// m_btn_reloadUserObjects
 			// 
-			this->reloadObjectDatabaseToolStripMenuItem->Name = L"reloadObjectDatabaseToolStripMenuItem";
-			this->reloadObjectDatabaseToolStripMenuItem->Size = System::Drawing::Size(199, 22);
-			this->reloadObjectDatabaseToolStripMenuItem->Text = L"Reload Object Database";
+			this->m_btn_reloadUserObjects->Name = L"m_btn_reloadUserObjects";
+			this->m_btn_reloadUserObjects->Size = System::Drawing::Size(199, 22);
+			this->m_btn_reloadUserObjects->Text = L"Reload User Objects";
+			this->m_btn_reloadUserObjects->Click += gcnew System::EventHandler(this, &MainGui::MainGui_ReloadUserObjects_Click);
+			// 
+			// m_btn_reloadDatabase
+			// 
+			this->m_btn_reloadDatabase->Name = L"m_btn_reloadDatabase";
+			this->m_btn_reloadDatabase->Size = System::Drawing::Size(199, 22);
+			this->m_btn_reloadDatabase->Text = L"Reload Object Database";
+			this->m_btn_reloadDatabase->Click += gcnew System::EventHandler(this, &MainGui::MainGui_ReloadDatabase_Click);
 			// 
 			// toolStripSeparator1
 			// 
 			this->toolStripSeparator1->Name = L"toolStripSeparator1";
 			this->toolStripSeparator1->Size = System::Drawing::Size(196, 6);
 			// 
-			// optionsToolStripMenuItem
+			// m_btn_options
 			// 
-			this->optionsToolStripMenuItem->Name = L"optionsToolStripMenuItem";
-			this->optionsToolStripMenuItem->Size = System::Drawing::Size(199, 22);
-			this->optionsToolStripMenuItem->Text = L"Options";
+			this->m_btn_options->Name = L"m_btn_options";
+			this->m_btn_options->Size = System::Drawing::Size(199, 22);
+			this->m_btn_options->Text = L"Options";
 			// 
 			// m_lbl_objSelectorStatus
 			// 
 			this->m_lbl_objSelectorStatus->BackColor = System::Drawing::SystemColors::ControlLightLight;
-			this->m_lbl_objSelectorStatus->Location = System::Drawing::Point(63, 141);
+			this->m_lbl_objSelectorStatus->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F));
+			this->m_lbl_objSelectorStatus->Location = System::Drawing::Point(114, 137);
 			this->m_lbl_objSelectorStatus->Name = L"m_lbl_objSelectorStatus";
-			this->m_lbl_objSelectorStatus->Size = System::Drawing::Size(259, 12);
+			this->m_lbl_objSelectorStatus->Size = System::Drawing::Size(259, 20);
 			this->m_lbl_objSelectorStatus->TabIndex = 0;
 			this->m_lbl_objSelectorStatus->Text = L"OBJECT_SELECTOR_STATUS";
 			this->m_lbl_objSelectorStatus->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// m_progressBarUpdater
+			// 
+			this->m_progressBarUpdater->Interval = 50;
+			this->m_progressBarUpdater->Tick += gcnew System::EventHandler(this, &MainGui::MainGui_Timer_UpdateProgressBar);
+			// 
+			// m_bw_databaseLoader
+			// 
+			this->m_bw_databaseLoader->DoWork += gcnew System::ComponentModel::DoWorkEventHandler(this, &MainGui::MainGui_DatabaseLoader_DoWork);
+			this->m_bw_databaseLoader->RunWorkerCompleted += gcnew System::ComponentModel::RunWorkerCompletedEventHandler(this, &MainGui::MainGui_DatabaseLoader_RunWorkerCompleted);
+			// 
+			// m_bw_objectLoader
+			// 
+			this->m_bw_objectLoader->DoWork += gcnew System::ComponentModel::DoWorkEventHandler(this, &MainGui::MainGui_ObjectLoader_DoWork);
+			this->m_bw_objectLoader->RunWorkerCompleted += gcnew System::ComponentModel::RunWorkerCompletedEventHandler(this, &MainGui::MainGui_ObjectLoader_RunWorkerCompleted);
 			// 
 			// MainGui
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(384, 311);
+			this->ClientSize = System::Drawing::Size(484, 311);
 			this->Controls->Add(this->m_lbl_objSelectorStatus);
 			this->Controls->Add(this->m_lbl_progressStatus);
 			this->Controls->Add(this->m_pb_progress);
@@ -266,10 +282,11 @@ namespace SMConverter {
 			this->Controls->Add(this->m_btn_folderDialog);
 			this->Controls->Add(this->m_menuStrip);
 			this->MainMenuStrip = this->m_menuStrip;
-			this->MinimumSize = System::Drawing::Size(400, 350);
+			this->MinimumSize = System::Drawing::Size(500, 350);
 			this->Name = L"MainGui";
 			this->ShowIcon = false;
 			this->Text = L"Scrap Mechanic Converter";
+			this->Shown += gcnew System::EventHandler(this, &MainGui::MainGui_Shown);
 			this->Resize += gcnew System::EventHandler(this, &MainGui::MainGui_Resize);
 			this->m_menuStrip->ResumeLayout(false);
 			this->m_menuStrip->PerformLayout();
@@ -278,10 +295,22 @@ namespace SMConverter {
 
 		}
 #pragma endregion
+		System::Void MainGui_Shown(System::Object^ sender, System::EventArgs^ e);
 		System::Void SelectedGenerator_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e);
 		System::Void PathTextBox_TextChanged(System::Object^ sender, System::EventArgs^ e);
 		System::Void FolderDialog_Click(System::Object^ sender, System::EventArgs^ e);
 		System::Void AboutButton_Click(System::Object^ sender, System::EventArgs^ e);
 		System::Void MainGui_Resize(System::Object^ sender, System::EventArgs^ e);
+		System::Void MainGui_Timer_UpdateProgressBar(System::Object^ sender, System::EventArgs^ e);
+		System::Void LoadObjectDatabase(const bool& should_reload);
+		System::Void MainGui_DatabaseLoader_DoWork(System::Object^ sender, System::ComponentModel::DoWorkEventArgs^ e);
+		System::Void MainGui_DatabaseLoader_RunWorkerCompleted(System::Object^ sender, System::ComponentModel::RunWorkerCompletedEventArgs^ e);
+		System::Void MainGui_ChangeGuiState(bool db_loaded, bool objlist_loaded, bool obj_converted);
+		System::Void MainGui_ReloadDatabase_Click(System::Object^ sender, System::EventArgs^ e);
+		System::Void LoadUserObjects();
+		System::Void UpdateObjectListStatus();
+		System::Void MainGui_ObjectLoader_DoWork(System::Object^ sender, System::ComponentModel::DoWorkEventArgs^ e);
+		System::Void MainGui_ObjectLoader_RunWorkerCompleted(System::Object^ sender, System::ComponentModel::RunWorkerCompletedEventArgs^ e);
+		System::Void MainGui_ReloadUserObjects_Click(System::Object^ sender, System::EventArgs^ e);
 	};
 }
